bfa8a87b1956c99f8eb69ea6c135cd36e2f54a1d
maden71 <kristian71m@gmail.com>
2022-05-21 16:28:49 +0300

    ex commit

--- src/JavaAdvanced/MaximumElement.java
+++ src/JavaAdvanced/MaximumElement.java
@@ -1,7 +1,36 @@
 package JavaAdvanced;
 
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
 public class MaximumElement {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            String[] input = scanner.nextLine().split("\\s+");
+            switch(input[0]) {
+                case "1": {
+                    int numberToPush = Integer.parseInt(input[1]);
+                    stack.push(numberToPush);
+                    break;
+                }
+                case "2": {
+                    stack.pop();
+                    break;
+                }
+                case "3":
+                    int maxN = Integer.MIN_VALUE;
+                    for (Integer num : stack) {
+                        if(num > maxN) {
+                            maxN = num;
+                        }
+                    }
+                    System.out.println(maxN);
+                    break;
+                }
+            }
 
-    }
+        }
 }
