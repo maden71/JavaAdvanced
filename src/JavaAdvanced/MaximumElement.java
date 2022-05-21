89eb480d26e3210c5311f1a275ee2c0215be9765
maden71 <kristian71m@gmail.com>
2022-05-21 16:33:10 +0300

    Update MaximumElement.java

--- src/JavaAdvanced/MaximumElement.java
+++ src/JavaAdvanced/MaximumElement.java
@@ -10,27 +10,25 @@ public class MaximumElement {
         ArrayDeque<Integer> stack = new ArrayDeque<>();
         for (int i = 0; i < n; i++) {
             String[] input = scanner.nextLine().split("\\s+");
-            switch(input[0]) {
-                case "1": {
+            switch (input[0]) {
+                case "1" -> {
                     int numberToPush = Integer.parseInt(input[1]);
                     stack.push(numberToPush);
-                    break;
                 }
-                case "2": {
+                case "2" -> {
                     stack.pop();
-                    break;
                 }
-                case "3":
+                case "3" -> {
                     int maxN = Integer.MIN_VALUE;
                     for (Integer num : stack) {
-                        if(num > maxN) {
+                        if (num > maxN) {
                             maxN = num;
                         }
                     }
                     System.out.println(maxN);
-                    break;
                 }
             }
-
         }
+
+    }
 }
