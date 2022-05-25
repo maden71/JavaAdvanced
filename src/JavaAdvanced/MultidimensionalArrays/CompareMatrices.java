b2bef700ccb12b898bb92832e98586fbe1d0c38b
maden71 <kristian71m@gmail.com>
2022-05-25 16:39:36 +0300

    changes

---
+++ src/JavaAdvanced/MultidimensionalArrays/CompareMatrices.java
@@ -0,0 +1,10 @@
+package JavaAdvanced.MultidimensionalArrays;
+
+import java.util.Scanner;
+
+public class CompareMatrices {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}
