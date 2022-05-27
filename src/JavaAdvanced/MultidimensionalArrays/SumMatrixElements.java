10193ec940ab3aae89fe248ad8286ae5d2ad442a
maden71 <kristian71m@gmail.com>
2022-05-27 09:52:42 +0300

    commit

---
+++ src/JavaAdvanced/MultidimensionalArrays/SumMatrixElements.java
@@ -0,0 +1,37 @@
+package JavaAdvanced.MultidimensionalArrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SumMatrixElements {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[][] matrix = readMatrix(scanner);
+        int sum = 0;
+        for (int[] row : matrix) {
+            for (int i : row) {
+                sum += i;
+            }
+        }
+        System.out.println(matrix.length);
+        System.out.println(matrix[0].length);
+        System.out.println(sum);
+    }
+    public static int[][] readMatrix(Scanner scanner) {
+        int[] input = Arrays
+                .stream(scanner.nextLine().split(",\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        int rows = input[0];
+        int cols = input[1];
+        int[][] matrix = new int[rows][cols];
+        for (int row = 0; row < rows; row++) {
+            int[] data = Arrays
+                    .stream(scanner.nextLine().split(",\\s+"))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+            matrix[row] = data;
+        }
+        return matrix;
+    }
+}
