fd4932784f37da9ef3a58a063ad8a4abf0c18ad6
maden71 <kristian71m@gmail.com>
2022-05-27 09:44:59 +0300

    problem commit

--- src/JavaAdvanced/MultidimensionalArrays/IntersectionofTwoMatrices.java
+++ src/JavaAdvanced/MultidimensionalArrays/IntersectionofTwoMatrices.java
@@ -6,23 +6,31 @@ import java.util.Scanner;
 public class IntersectionofTwoMatrices {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
-    }
-    public static int[][] readMatrix(Scanner scanner) {
-        int[] input = Arrays
-                .stream(scanner.nextLine().split("\\s+"))
-                .mapToInt(Integer::parseInt)
-                .toArray();
-        int rows = input[0];
-        int cols = input[1];
-        int[][] matrix = new int[rows][cols];
-        for (int row = 0; row < rows; row++) {
-            int[] data = Arrays
-                    .stream(scanner.nextLine().split("\\s+"))
-                    .mapToInt(Integer::parseInt)
-                    .toArray();
-            matrix[row] = data;
+        int rows = Integer.parseInt(scanner.nextLine());
+        int cols = Integer.parseInt(scanner.nextLine());
+        String[][] firstMatrix = new String[rows][cols];
+        for (int row = 0; row < firstMatrix.length ; row++) {
+            String[] data = scanner.nextLine().split("\\s+");
+            firstMatrix[row] = data;
         }
-        return matrix;
+        String[][] secondMatrix = new String[rows][cols];
+        for (int row = 0; row < secondMatrix.length; row++) {
+            String[] data = scanner.nextLine().split("\\s+");
+            secondMatrix[row] = data;
+        }
+        compareAndPrintMatrix(firstMatrix,secondMatrix);
+    }
+
+    private static void compareAndPrintMatrix(String[][] firstMatrix, String[][] secondMatrix) {
+        for (int row = 0; row < firstMatrix.length; row++) {
+            for (int col = 0; col < firstMatrix[row].length ; col++) {
+                if(!firstMatrix[row][col].equals(secondMatrix[row][col])) {
+                    System.out.print("* ");
+                }else {
+                    System.out.print(firstMatrix[row][col] + " ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
