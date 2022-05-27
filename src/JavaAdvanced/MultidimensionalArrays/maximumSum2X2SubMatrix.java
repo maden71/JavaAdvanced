3aecc7521859d392e726e00e03c4a6df39e50c44
maden71 <kristian71m@gmail.com>
2022-05-27 10:27:19 +0300

    problem commit

--- src/JavaAdvanced/MultidimensionalArrays/maximumSum2X2SubMatrix.java
+++ src/JavaAdvanced/MultidimensionalArrays/maximumSum2X2SubMatrix.java
@@ -11,7 +11,7 @@ public class maximumSum2X2SubMatrix {
         int maxSum = 0;
         for (int row = 0; row < matrix.length - 1; row++) {
             int sum = 0;
-            for (int col = 0; col <matrix[row].length ; col+=2) {
+            for (int col = 0; col <matrix[row].length - 1 ; col++) {
                 sum += matrix[row][col]  + matrix[row][col+1] + matrix[row+1][col] + matrix[row+1][col+1];
                 if(sum > maxSum) {
                     maxSum = sum;
