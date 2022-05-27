cb434a146ec5b42ae730232873cfee6e6c63502b
maden71 <kristian71m@gmail.com>
2022-05-27 09:59:26 +0300

    commit

---
+++ src/JavaAdvanced/MultidimensionalArrays/MaximumSum2X2Submatrix.java
@@ -0,0 +1,12 @@
+package JavaAdvanced.MultidimensionalArrays;
+
+import java.util.Scanner;
+
+public class MaximumSum2X2Submatrix {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+    
+
+}
