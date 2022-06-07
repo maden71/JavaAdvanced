dc7130489b6db57629f777dfe7b71375b5689f5e
maden71 <kristian71m@gmail.com>
2022-06-07 19:58:11 +0300

    commit

--- src/JavaAdvanced/DefiningClasses/lab/BankAccount/Bank.java
+++ src/JavaAdvanced/DefiningClasses/lab/BankAccount/Bank.java
@@ -1,4 +1,21 @@
 package JavaAdvanced.DefiningClasses.lab.BankAccount;
 
 public class Bank {
+    private int nextId = 1;
+    private int id;
+    private double balance;
+    private double interestRate = 0.02;
+
+
+    public Bank() {
+        this.id = nextId;
+        nextId++;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
+    double getInterestRate(int years) {
+        return this.interestRate * years * this.balance;
+    }
 }
--- src/JavaAdvanced/DefiningClasses/lab/BankAccount/Main.java
+++ src/JavaAdvanced/DefiningClasses/lab/BankAccount/Main.java
@@ -1,7 +1,28 @@
 package JavaAdvanced.DefiningClasses.lab.BankAccount;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        String[] input = scanner.nextLine().split("\\s+");
+        Map<Integer,Integer> accounts = new HashMap<>();
+        while (!input[0].equals("End")) {
+            switch (input[0]) {
+                case "Create" :
+                    
+                    break;
+                case "Deposit" :
+                    break;
+                case "SetInterest" :
+                    break;
+                case "GetInterest" :
+                    break;
+            }
+            input = scanner.nextLine().split("\\s+");
+        }
+
     }
 }
