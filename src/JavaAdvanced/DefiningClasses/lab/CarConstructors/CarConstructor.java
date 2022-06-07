9de68476a4205a3e0d94d28d7e74fa0286131246
maden71 <kristian71m@gmail.com>
2022-06-07 18:32:24 +0300

    solution commit

--- src/JavaAdvanced/DefiningClasses/lab/CarConstructors/CarConstructor.java
+++ src/JavaAdvanced/DefiningClasses/lab/CarConstructors/CarConstructor.java
@@ -1,6 +1,47 @@
 package JavaAdvanced.DefiningClasses.lab.CarConstructors;
 
+import JavaAdvanced.DefiningClasses.lab.CarInfo.Car;
+
 public class CarConstructor {
+    private String brand;
+    private String model;
+    private int horsePower;
 
 
+    public CarConstructor(String brand, String model, int horsePower) {
+       this.brand = brand;
+       this.model = model;
+       this.horsePower = horsePower;
+    }
+    public CarConstructor(String brand) {
+        this(brand,"unknown", -1);
+    }
+
+    String carInfo() {
+        return String.format("The car is: %s %s - %d HP.",getBrand(),getModel(),getHorsePower());
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
 }
--- src/JavaAdvanced/DefiningClasses/lab/CarConstructors/Main.java
+++ src/JavaAdvanced/DefiningClasses/lab/CarConstructors/Main.java
@@ -5,6 +5,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        int n = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < n; i++) {
+            String[] data = scanner.nextLine().split("\\s+");
+            CarConstructor car;
+            String brand = data[0];
+            if (data.length > 1) {
+                String model = data[1];
+                int horsePower = Integer.parseInt(data[2]);
+                car = new CarConstructor(brand, model, horsePower);
+            } else {
+                car = new CarConstructor(brand);
+            }
+            System.out.println(car.carInfo());
+        }
     }
 }
