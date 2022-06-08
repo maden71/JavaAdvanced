7684afb5426a206c5b3ace72ccec594e769b5fb5
maden71 <kristian71m@gmail.com>
2022-06-08 20:08:33 +0300

    Create CitiesContinentCountry.java

---
+++ src/JavaAdvanced/SetsAndMapsAdv/CitiesContinentCountry.java
@@ -0,0 +1,10 @@
+package JavaAdvanced.SetsAndMapsAdv;
+
+import java.util.Scanner;
+
+public class CitiesContinentCountry {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}
