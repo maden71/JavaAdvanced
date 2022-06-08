5897dba71f60480cd39438b82770fc81e2c4c462
maden71 <kristian71m@gmail.com>
2022-06-08 20:17:37 +0300

    solution commit

--- src/JavaAdvanced/SetsAndMapsAdv/CitiesContinentCountry.java
+++ src/JavaAdvanced/SetsAndMapsAdv/CitiesContinentCountry.java
@@ -1,10 +1,28 @@
 package JavaAdvanced.SetsAndMapsAdv;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class CitiesContinentCountry {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        int n = Integer.parseInt(scanner.nextLine());
+        Map<String,Map<String,List<String>>> continents = new LinkedHashMap<>();
+        while(n-- > 0) {
+            String[] data = scanner.nextLine().split("\\s+");
+            String continent = data[0];
+            String country = data[1];
+            String city = data[2];
+            continents.putIfAbsent(continent,new LinkedHashMap<>());
+            continents.get(continent).putIfAbsent(country,new ArrayList<>());
+            continents.get(continent).get(country).add(city);
+
+        }
+        for (Map.Entry<String, Map<String, List<String>>> entry : continents.entrySet()) {
+            System.out.printf("%s:%n",entry.getKey());
+            entry.getValue().entrySet().forEach(e -> {
+                System.out.printf("%s -> %s%n",e.getKey(),String.join(", ",e.getValue()));
+            });
+
+        }
     }
 }
