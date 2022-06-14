7ddfc1e8b3815ae4a0b8eb9f5a2efca988711882
maden71 <kristian71m@gmail.com>
2022-06-14 14:14:30 +0300

    solution commit

--- src/JavaAdvanced/ExamPrep/retake14april/OSPlanning.java
+++ src/JavaAdvanced/ExamPrep/retake14april/OSPlanning.java
@@ -1,5 +1,8 @@
 package JavaAdvanced.ExamPrep.retake14april;
 
+import com.sun.jdi.ArrayReference;
+
+import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -15,6 +18,28 @@ public class OSPlanning {
                 .mapToInt(Integer::parseInt)
                 .toArray();
         int thirdInput = Integer.parseInt(scanner.nextLine());
+
+        //task - stack thread - queue
+        ArrayDeque<Integer> tasks = new ArrayDeque<>();
+        ArrayDeque<Integer> threads = new ArrayDeque<>();
+
+        Arrays.stream(firstInput).forEach(tasks::push);
+        Arrays.stream(secondInput).forEach(threads::offer);
+
+        while(!threads.isEmpty()) {
+            int task = tasks.peek();
+            int thread = threads.peek();
+            if(task == thirdInput) {
+                System.out.printf("Thread with value %d killed task %d%n",thread,task);
+                threads.stream().forEach(e -> System.out.printf("%d ",e));
+                return;
+            } else if(thread >= task) {
+                tasks.pop();
+                threads.poll();
+            }else if(thread < task) {
+                threads.poll();
+            }
+        }
 
     }
 }
