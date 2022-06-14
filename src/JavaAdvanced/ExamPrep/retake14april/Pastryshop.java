a432893240a5bd54c3fd5ff4773b352a290b4bd5
maden71 <kristian71m@gmail.com>
2022-06-14 22:28:09 +0300

    Update Pastryshop.java

--- src/JavaAdvanced/ExamPrep/retake14april/Pastryshop.java
+++ src/JavaAdvanced/ExamPrep/retake14april/Pastryshop.java
@@ -1,9 +1,27 @@
 package JavaAdvanced.ExamPrep.retake14april;
 
+import java.lang.reflect.Array;
+import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Pastryshop {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int[] firstLine = Arrays
+                .stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        int[] secondLine = Arrays
+                .stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        //liquid - queue ingredient - stack
+        ArrayDeque<Integer> liquids = new ArrayDeque<>();
+        ArrayDeque<Integer> ingredients = new ArrayDeque<>();
+
+        Arrays.stream(firstLine).forEach(liquids::offer);
+        Arrays.stream(secondLine).forEach(ingredients::push);
+        
     }
 }
