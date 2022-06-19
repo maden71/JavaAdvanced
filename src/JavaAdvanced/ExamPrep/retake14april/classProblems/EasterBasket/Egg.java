23a2fb07ac6436446d3a1e81b314c46e302ad168
maden71 <kristian71m@gmail.com>
2022-06-19 22:10:50 +0300

    Update Egg.java

--- src/JavaAdvanced/ExamPrep/retake14april/classProblems/EasterBasket/Egg.java
+++ src/JavaAdvanced/ExamPrep/retake14april/classProblems/EasterBasket/Egg.java
@@ -1,4 +1,9 @@
 package JavaAdvanced.ExamPrep.retake14april.classProblems.EasterBasket;
 
 public class Egg {
+    private String color;
+    private int strength;
+    private String shape;
+
+    
 }
