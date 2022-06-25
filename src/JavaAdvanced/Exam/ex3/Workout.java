b7a17a00192c868f01c788cb79eec1a39b03677d
maden71 <kristian71m@gmail.com>
2022-06-25 09:06:06 +0300

    commit

---
+++ src/JavaAdvanced/Exam/ex3/Workout.java
@@ -0,0 +1,10 @@
+package JavaAdvanced.Exam.ex3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Workout {
+    private String type;
+    private int exerciseCount;
+    List<Exercise> exercises = new ArrayList<>();
+}
