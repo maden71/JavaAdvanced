355e1856b41604824cf16f259eb18a14e0d4e941
maden71 <kristian71m@gmail.com>
2022-07-16 16:48:10 +0300

    commit

--- src/JavaOOP/StudentSystem/Main.java
+++ src/JavaOOP/StudentSystem/Main.java
@@ -3,5 +3,6 @@ package JavaOOP.StudentSystem;
 public class Main {
     public static void main(String[] args) {
 
+
     }
 }
--- src/JavaOOP/StudentSystem/Student.java
+++ src/JavaOOP/StudentSystem/Student.java
@@ -1,5 +1,14 @@
 package JavaOOP.StudentSystem;
 
 public class Student {
-    
+    private static String studentName;
+    private static int studentAge;
+    private static double studentGrade;
+
+    public void create(String name, int age, double grade) {
+        this.studentName = name;
+        this.studentAge = age;
+        this.
+    }
+
 }
