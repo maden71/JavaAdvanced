807467f62b3ed9bb6de808db5bc4d18af6dc5972
Kristian Mitev <kristian71m@gmail.com>
2022-08-13 12:22:48 +0300

    commit

---
+++ src/ExamPrep2/src/main/java/restaurant/entities/healthyFoods/interfaces/Food.java
@@ -0,0 +1,20 @@
+package restaurant.entities.healthyFoods.interfaces;
+
+public class Food implements HealthyFood {
+    
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public double getPortion() {
+        return 0;
+    }
+
+    @Override
+    public double getPrice() {
+        return 0;
+    }
+}
